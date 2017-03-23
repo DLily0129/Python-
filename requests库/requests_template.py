630dbcdd6117c7b44850d24b08755e8b86428d7c
DLily0129 <18800205316@163.com>
2017-03-23 23:46:51 +0800

    使用方法

--- requests库/requests_template.py
+++ requests库/requests_template.py
@@ -2,7 +2,7 @@ import requests
 
 def get_HTML_Text(url):
     try:
-        r = requests.get(url,timeout=30)
+        r = requests.get(url, timeout=30)
         r.raise_for_status()
         r.encoding = r.apparent_encoding
         return r.text
@@ -10,5 +10,5 @@ def get_HTML_Text(url):
         return "产生异常"
 
 if __name__=="__main__":
-    url = "http:/baidu.com"
+    url = "https://www.amazon.cn/gp/product/B01M8L5Z3Y"
     print(get_HTML_Text(url))
