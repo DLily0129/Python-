673007db39b8b1c58f1155b3d527805da8a323c1
DLily0129 <18800205316@163.com>
2017-03-25 23:58:38 +0800

    正则表达式

--- test4.py
+++ test4.py
@@ -1,13 +1,9 @@
-num=int(input("请输入一个数:"))
-if num == 1 :
-    print("该数不是素数")
-else :
-    for i in range(2,num) :
-        if num % i == 0 :
-            print("该数不是素数")
-            break
-    else :     #当循环被 break 结束时, else 不执行;当循环正常结束时,即没有被break, else 被执行
-        print("该数是素数")
-print("////////////////////////")
-
+import re
+r = r'\d{3}-\d{8}'
+match = re.search(r, '123,010-12345678,027-12345678,')
+print(type(match))
+if match:
+    print(match.group())
+else:
+    print("not match")
 
